6a7f5bc4530d4ed329a5bb3db571abf40f9010d1
JonKoala <victor.santana.lemos@gmail.com>
2019-08-28 19:03:47 -0300

    corrige falta de icones do vuetify

--- webpack.common.js
+++ webpack.common.js
@@ -27,6 +27,10 @@ module.exports = {
       {
         test: /\.vue$/,
         loader: 'vue-loader'
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        loader: 'file-loader'
       }
     ]
   },
